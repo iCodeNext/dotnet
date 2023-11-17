385e8136dda95f463d6f751a3df9338da383a927
MohammadKarimi <mha.karimi>
2023-11-17 14:57:13 +0330

    complete synchronization context

--- iCodeNext/SynchronizationContext/Program.cs
+++ iCodeNext/SynchronizationContext/Program.cs
@@ -1,18 +1,31 @@
-﻿namespace SynchronizationContext;
-public class Program
+﻿public class Program
 {
     public static async Task Main(string[] args)
-     => await MethodA();
+    {
+
+        SynchronizationContext.SetSynchronizationContext(new MyContext());
+        await MethodA();
+        Console.WriteLine("Method A");
+    }
 
     public static async Task MethodA()
     {
-        await MethodB();
+        await MethodB().ConfigureAwait(false);
         Console.WriteLine("Method B");
     }
 
     public static async Task MethodB()
     {
-        await Task.Delay(1000);
+        await Task.Delay(1000).ConfigureAwait(false);
         Console.WriteLine("Method C");
+    }
+}
+
+public class MyContext : SynchronizationContext
+{
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        Console.WriteLine("MyContext!");
+        base.Post(d, state);
     }
 }
