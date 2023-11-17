c9895ac8293d75550b9437ac1597d18c0d5720b4
MohammadKarimi <mha.karimi>
2023-11-17 11:16:33 +0330

    Add synchronization context simple application

---
+++ iCodeNext/SynchronizationContext/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SynchronizationContext;
+public class Program
+{
+    public static async Task Main(string[] args)
+     => await MethodA();
+
+    public static async Task MethodA()
+    {
+        await MethodB();
+        Console.WriteLine("Method B");
+    }
+
+    public static async Task MethodB()
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("Method C");
+    }
+}
