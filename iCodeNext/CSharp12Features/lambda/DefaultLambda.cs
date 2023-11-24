fd59df279e3e49ece80351ce646df2a19b999eb8
mohammadKarimi <mha.karimi@gmail.com>
2023-11-24 17:36:07 +0330

    Change Nothing

--- iCodeNext/CSharp12Features/lambda/DefaultLambda.cs
+++ iCodeNext/CSharp12Features/lambda/DefaultLambda.cs
@@ -4,15 +4,15 @@ public class Program
 {
     public void Main(string[] args)
     {
-        var action = (int x = 75) => {  };
+        var action = (int x = 75) => { };
 
         var action1 = (params string[] names) =>
         {
 
         };
 
-        var action2 = (int number) => {   };
- 
+        var action2 = (int number) => { };
+
     }
 
     public delegate int ProcessMethod(int number);
