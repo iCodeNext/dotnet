c23eef7082048116b4c7bdccc77989f8f09cd7ca
mohammadKarimi <mha.karimi@gmail.com>
2023-11-27 14:24:47 +0330

    Add GenericRepository

---
+++ iCodeNext/WithIn.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace WithIn.Domain.Repositories;
+
+public interface IGenericRepository<T>
+{
+    void Add(T entity);
+    Task<T> FindAsync();
+    Task<T> FirstOrDefault(Expression<Func<T, bool>> expression);
+    void Remove(T entity);
+}
