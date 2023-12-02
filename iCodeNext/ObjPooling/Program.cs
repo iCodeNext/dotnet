5c88b5da751dcc69e0d8b4364bf59f69887b92c5
mohammadKarimi <mha.karimi@gmail.com>
2023-12-02 21:52:26 +0330

    Add IResettable Interface

--- iCodeNext/ObjPooling/Program.cs
+++ iCodeNext/ObjPooling/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using Microsoft.Extensions.ObjectPool;
+using System.Text;
+using System.Xml.Linq;
+namespace ObjPool;
+
+ 
+    public class BufferConsumer(ObjectPool<ReusableBuffer> bufferPool)
+    {
+        public void DoSomething()
+        {
+            var buffer = bufferPool.Get();
+            try
+            {
+               // TODO
+            }
+            finally
+            {
+                bufferPool.Return(buffer);
+            }
+        }
+    }
+
+        public class ReusableBuffer : IResettable
+        {
+            public bool TryReset()
+            {
+                // Reset State before it is returned to the pool.
+                return true;
+            }
+        }
+
+
