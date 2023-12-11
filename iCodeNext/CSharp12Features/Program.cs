b557a4a7ca1bf877f13d043a949396de6fbdc0e5
mohammadKarimi <mha.karimi@gmail.com>
2023-12-11 14:45:38 +0330

    SOC vs SRP

--- iCodeNext/CSharp12Features/Program.cs
+++ iCodeNext/CSharp12Features/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+using System.Net.Mail;
+
+public class NotificationService
+{
+    public void SendEmail(string email, string reciever)
+    {
+        SmtpClient smtpClient = new SmtpClient();
+        // todo
+        Console.WriteLine("Send Email!");
+    }
+
+    public void SendSms(string message, string reciever)
+    {
+        HttpClient httpClient = new HttpClient();
+        //todo
+        Console.WriteLine("Send Sms!");
+    }
+}
+
+public class UserHandler(NotificationService notificationService)
+{
+    public void Handle()
+    {
+        notificationService.SendSms("message", "0939....");
+    }
+}
