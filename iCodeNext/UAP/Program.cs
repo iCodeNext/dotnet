d1198e70b683e3ced85df5e85e5ffef6ccb1a5c1
mohammadKarimi <mha.karimi@gmail.com>
2023-12-13 22:34:29 +0330

    Add Program

--- iCodeNext/UAP/Program.cs
+++ iCodeNext/UAP/Program.cs
@@ -1,3 +1,6 @@
-﻿Console.WriteLine("I am trying to Find Pakcages!!");
+﻿using System;
+
+Console.WriteLine("I am trying to Find Pakcages!!");
 Console.WriteLine("I have found 4 Outdated packages");
-Console.WriteLine("All Packages are updated :)"); 
+Console.WriteLine("All Packages are updated :)");
+
