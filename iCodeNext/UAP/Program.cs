f2ff6a0ce4d87c504d9d2d81b3b8eef9bd07bcfc
mohammadKarimi <mha.karimi@gmail.com>
2023-12-14 14:16:31 +0330

    Remove UAP Project

--- iCodeNext/UAP/Program.cs
+++ iCodeNext/UAP/Program.cs
@@ -1,6 +1,1 @@
-﻿using System;
-
-Console.WriteLine("I am trying to Find Pakcages!!");
-Console.WriteLine("I have found 4 Outdated packages");
-Console.WriteLine("All Packages are updated :)");
-
+﻿
