5f899b8df5a4b5c50a1a6778b5750c0653048b87
mohammadKarimi <mha.karimi@gmail.com>
2023-12-23 12:40:15 +0330

    Add Common Coupling and Content Coupling Add Image of Coupling

---
+++ iCodeNext/TypesOfCoupling/CommongCoupling.cs
@@ -0,0 +1,32 @@
+﻿namespace TypesOfCoupling;
+
+
+file class User
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+file class UserService
+{
+    public void InsertUser(User user)
+    {
+        GlobalData.SequenceId += 1;
+        //TODO
+    }
+}
+
+public class GlobalData
+{
+    public static int SequenceId = 0;
+}
+
+file class OrderService
+{
+    public void AddOrder()
+    {
+        GlobalData.SequenceId += 1;
+        //TODO
+    }
+}
---
+++ iCodeNext/TypesOfCoupling/ContentCoupling.cs
@@ -0,0 +1,37 @@
+﻿namespace TypesOfCoupling;
+
+
+
+file class User
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+file class UserService
+{
+    public IReadOnlyList<User> GetUsers()
+    {
+        var result = new List<User>();
+
+        // result = DbContext.Users.Where(x=> x.Name == "Mohammad").ToList();
+
+        return result;
+    }
+}
+
+
+file class GiftGenerator(UserService userService)
+{
+    private readonly UserService _userService = userService;
+
+    public void Generate()
+    {
+        var users = _userService.GetUsers();
+        foreach (var user in users)
+        {
+            //Create Discount Code
+        }
+    }
+}
