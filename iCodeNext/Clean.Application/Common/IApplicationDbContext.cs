5332da352b2f5e5cc5cc27f481b96b3fbb680000
mohammadKarimi <mha.karimi@gmail.com>
2024-01-03 13:29:42 +0330

    Add Clean Example

---
+++ iCodeNext/Clean.Application/Common/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Clean.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clean.Services.Context;
+
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; }
+
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
