9e6ae56919bffb46ee11aef3b4b141df14797e34
mohammadKarimi <mha.karimi@gmail.com>
2024-02-15 19:57:58 +0330

    Add Transactions

--- iCodeNext/QueryProviderExample/ApplicationDbContext.cs
+++ iCodeNext/QueryProviderExample/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QueryProviderExample;
 
@@ -6,15 +8,26 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
     : DbContext(options)
 {
     public DbSet<Post> Posts { get; set; }
+    public DbSet<Auther> Authors { get; set; }
 }
-
 
 public class Post
 {
     public int Id { get; set; }
+
+    [MaxLength(10)]
     public string Title { get; set; }
     public string Description { get; set; }
-    public int Code { get; set; }
+
+    [ForeignKey(nameof(AutherId))]
+    public Auther Auther { get; set; }
+    public int AutherId { get; set; }
 }
 
-public record GetPostDto(int Id, string Title, string Description);
+public class Auther
+{
+    public int Id { get; set; }
+
+    [MaxLength(10)]
+    public string Name { get; set; }
+}
--- iCodeNext/QueryProviderExample/Program.cs
+++ iCodeNext/QueryProviderExample/Program.cs
@@ -1,12 +1,13 @@
-﻿using Azure.Core;
+﻿#nullable disable
+
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using QueryProviderExample;
-using System.Globalization;
+
+var connection = "Server=DESKTOP-TVCSFN3\\MHA;Database=iCodeNext;Trusted_Connection=True;Encrypt=false";
 
 var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-                           .UseSqlServer("Server=DESKTOP-TVCSFN3\\MHA;Database=iCodeNext;Trusted_Connection=True;Encrypt=false")
+                           .UseSqlServer(connection)
                            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
                            .EnableSensitiveDataLogging()
                            .Options;
@@ -15,9 +16,83 @@ ApplicationDbContext context = new(dbContextOptions);
 context.Database.EnsureDeleted();
 context.Database.EnsureCreated();
 
+//context.Authors.Add(new Auther { Name = "Mohammad" });
+//context.Authors.Add(new Auther { Name = "Mohammad_1" });
 
-await context.Posts.Where(x => x.Id == 1)
-                   .Select(x => new GetPostDto(x.Id, x.Title, x.Description))
-                   .FirstOrDefaultAsync(x => x.Id == 1);
- 
-await context.Posts.Select(x => new GetPostDto(x.Id, x.Title,"")).FirstOrDefaultAsync(x => x.Id == 1);
+//context.SaveChanges();
+
+//#
+//context.Authors.Add(new Auther { Name = "Mohammad" });
+//context.Authors.Add(new Auther { Name = "Mohammad_1111" });
+//context.SaveChanges();
+
+//#
+//var auther = new Auther { Name = "Mohammad" };
+//context.Authors.Add(auther);
+//context.SaveChanges();
+//context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books" });
+//context.SaveChanges();
+
+//#
+//var auther = new Auther { Name = "Mohammad" };
+//context.Authors.Add(auther);
+//context.SaveChanges();
+//context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books_123" });
+//context.SaveChanges();
+
+
+//#
+//using var transaction = context.Database.BeginTransaction();
+//try
+//{
+
+//	var auther = new Auther { Name = "Mohammad" };
+//	context.Authors.Add(auther);
+//	context.SaveChanges();
+//	context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books_123" });
+//	context.SaveChanges();
+
+//	transaction.Commit();
+//}
+//catch (Exception)
+//{
+//	transaction.Rollback();
+//}
+
+//#
+using var transaction = context.Database.BeginTransaction();
+var auther = new Auther { Name = "Mohammad" };
+var post = new Post { Title = "C# Books_123" };
+try
+{
+    context.Authors.Add(auther);
+    context.SaveChanges();
+
+    transaction.CreateSavepoint("Auther_Added");
+
+    post.AutherId = auther.Id;
+    context.Posts.Add(post);
+    context.SaveChanges();
+
+    transaction.Commit();
+}
+catch (Exception)
+{
+    post.Title = "C# New";
+    transaction.RollbackToSavepoint("Auther_Added");
+    context.Posts.Add(post);
+    context.SaveChanges();
+
+    transaction.Commit();
+}
+
+
+//#
+
+//using var sqlConnection = new SqlTransaction();
+//sqlConnection.Open();
+
+//using var transaction = sqlConnection.BeginTransaction();
+
+
+Console.ReadKey();
