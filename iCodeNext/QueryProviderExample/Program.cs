5ca537744e3de921b36d545e1a30e5d2bfe13455
mohammadKarimi <mha.karimi@gmail.com>
2024-02-20 16:06:48 +0330

    Strategy Pattern

--- iCodeNext/QueryProviderExample/Program.cs
+++ iCodeNext/QueryProviderExample/Program.cs
@@ -1,21 +1,21 @@
 ﻿#nullable disable
 
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using QueryProviderExample;
-using System.Transactions;
+//using Microsoft.Data.SqlClient;
+//using Microsoft.EntityFrameworkCore;
+//using QueryProviderExample;
+//using System.Transactions;
 
-var connection = "Server=DESKTOP-TVCSFN3\\MHA;Database=iCodeNext;Trusted_Connection=True;Encrypt=false";
+//var connection = "Server=DESKTOP-TVCSFN3\\MHA;Database=iCodeNext;Trusted_Connection=True;Encrypt=false";
 
-var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-                           .UseSqlServer(connection)
-                           .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
-                           .EnableSensitiveDataLogging()
-                           .Options;
+//var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+//                           .UseSqlServer(connection)
+//                           .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+//                           .EnableSensitiveDataLogging()
+//                           .Options;
 
-ApplicationDbContext context = new(dbContextOptions);
-context.Database.EnsureDeleted();
-context.Database.EnsureCreated();
+//ApplicationDbContext context = new(dbContextOptions);
+//context.Database.EnsureDeleted();
+//context.Database.EnsureCreated();
 
 //context.Authors.Add(new Auther { Name = "Mohammad" });
 //context.Authors.Add(new Auther { Name = "Mohammad_1" });
@@ -110,4 +110,81 @@ context.Database.EnsureCreated();
 
 //    }
 //}
-Console.ReadKey();
+//Console.ReadKey();
+
+
+
+
+
+
+using System.ComponentModel.Design;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var key = Console.ReadLine();
+        IPrintStrategy strategy = PrintFactory.GetInstance(key);
+        strategy.Print();
+    }
+}
+
+public static class PrintFactory
+{
+    public static IPrintStrategy GetInstance(string key)
+    {
+        if (key == "A" || key == "Z")
+        {
+            return new AStrategy();
+        }
+        else if (key == "B")
+        {
+            return new BStrategy();
+        }
+        else if (key == "C")
+        {
+            return new CStrategy();
+        }
+        else
+        {
+            return new DStrategy();
+        }
+    }
+}
+
+public interface IPrintStrategy
+{
+    void Print();
+}
+
+public class AStrategy : IPrintStrategy
+{
+    public void Print()
+    {
+        Console.WriteLine("AAA");
+    }
+}
+
+public class BStrategy : IPrintStrategy
+{
+    public void Print()
+    {
+        Console.WriteLine("BBB");
+    }
+}
+
+public class CStrategy : IPrintStrategy
+{
+    public void Print()
+    {
+        Console.WriteLine("CCC");
+    }
+}
+
+public class DStrategy : IPrintStrategy
+{
+    public void Print()
+    {
+        Console.WriteLine("DDD");
+    }
+}
