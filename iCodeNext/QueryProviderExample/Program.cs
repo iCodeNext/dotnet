a7f662706a220785fb19b5f46f45a7800eba84ba
mohammadKarimi <mha.karimi@gmail.com>
2024-02-20 16:43:18 +0330

    Transactions

--- iCodeNext/QueryProviderExample/Program.cs
+++ iCodeNext/QueryProviderExample/Program.cs
@@ -1,21 +1,23 @@
 ﻿#nullable disable
 
-//using Microsoft.Data.SqlClient;
-//using Microsoft.EntityFrameworkCore;
-//using QueryProviderExample;
-//using System.Transactions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using QueryProviderExample;
+using System.Transactions;
 
-//var connection = "Server=DESKTOP-TVCSFN3\\MHA;Database=iCodeNext;Trusted_Connection=True;Encrypt=false";
+var connection = "Server=DESKTOP-TVCSFN3\\MHA;Database=iCodeNext;Trusted_Connection=True;Encrypt=false";
 
-//var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-//                           .UseSqlServer(connection)
-//                           .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
-//                           .EnableSensitiveDataLogging()
-//                           .Options;
+var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                           .UseSqlServer(connection)
+                           .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                           .EnableSensitiveDataLogging()
+                           .Options;
 
-//ApplicationDbContext context = new(dbContextOptions);
-//context.Database.EnsureDeleted();
-//context.Database.EnsureCreated();
+ApplicationDbContext context = new(dbContextOptions);
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
+
+
 
 //context.Authors.Add(new Auther { Name = "Mohammad" });
 //context.Authors.Add(new Auther { Name = "Mohammad_1" });
@@ -31,6 +33,7 @@
 //var auther = new Auther { Name = "Mohammad" };
 //context.Authors.Add(auther);
 //context.SaveChanges();
+
 //context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books" });
 //context.SaveChanges();
 
@@ -38,6 +41,7 @@
 //var auther = new Auther { Name = "Mohammad" };
 //context.Authors.Add(auther);
 //context.SaveChanges();
+
 //context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books_123" });
 //context.SaveChanges();
 
@@ -50,7 +54,7 @@
 //	var auther = new Auther { Name = "Mohammad" };
 //	context.Authors.Add(auther);
 //	context.SaveChanges();
-//	context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books_123" });
+//	context.Posts.Add(new Post { AutherId = auther.Id, Title = "C# Books" });
 //	context.SaveChanges();
 
 //	transaction.Commit();
@@ -61,31 +65,31 @@
 //}
 
 //#
-//using var transaction = context.Database.BeginTransaction();
-//var auther = new Auther { Name = "Mohammad" };
-//var post = new Post { Title = "C# Books_123" };
-//try
-//{
-//    context.Authors.Add(auther);
-//    context.SaveChanges();
+using var transaction = context.Database.BeginTransaction();
+var auther = new Auther { Name = "Mohammad" };
+var post = new Post { Title = "C# Books_123" };
+try
+{
+    context.Authors.Add(auther);
+    context.SaveChanges();
 
-//    transaction.CreateSavepoint("Auther_Added");
+    transaction.CreateSavepoint("Auther_Added");
 
-//    post.AutherId = auther.Id;
-//    context.Posts.Add(post);
-//    context.SaveChanges();
+    post.AutherId = auther.Id;
+    context.Posts.Add(post);
+    context.SaveChanges();
 
-//    transaction.Commit();
-//}
-//catch (Exception)
-//{
-//    post.Title = "C# New";
-//    transaction.RollbackToSavepoint("Auther_Added");
-//    context.Posts.Add(post);
-//    context.SaveChanges();
+    transaction.Commit();
+}
+catch (Exception)
+{
+    post.Title = "C# New";
+    transaction.RollbackToSavepoint("Auther_Added");
+    context.Posts.Add(post);
+    context.SaveChanges();
 
-//    transaction.Commit();
-//}
+    transaction.Commit();
+}
 
 //#
 //using (var scope = new TransactionScope(
@@ -110,81 +114,8 @@
 
 //    }
 //}
-//Console.ReadKey();
+Console.ReadKey();
 
 
 
 
-
-
-using System.ComponentModel.Design;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var key = Console.ReadLine();
-        IPrintStrategy strategy = PrintFactory.GetInstance(key);
-        strategy.Print();
-    }
-}
-
-public static class PrintFactory
-{
-    public static IPrintStrategy GetInstance(string key)
-    {
-        if (key == "A" || key == "Z")
-        {
-            return new AStrategy();
-        }
-        else if (key == "B")
-        {
-            return new BStrategy();
-        }
-        else if (key == "C")
-        {
-            return new CStrategy();
-        }
-        else
-        {
-            return new DStrategy();
-        }
-    }
-}
-
-public interface IPrintStrategy
-{
-    void Print();
-}
-
-public class AStrategy : IPrintStrategy
-{
-    public void Print()
-    {
-        Console.WriteLine("AAA");
-    }
-}
-
-public class BStrategy : IPrintStrategy
-{
-    public void Print()
-    {
-        Console.WriteLine("BBB");
-    }
-}
-
-public class CStrategy : IPrintStrategy
-{
-    public void Print()
-    {
-        Console.WriteLine("CCC");
-    }
-}
-
-public class DStrategy : IPrintStrategy
-{
-    public void Print()
-    {
-        Console.WriteLine("DDD");
-    }
-}
