6fee054f484f5eea44df8b42f8f75b6509ae1aee
mohammadKarimi <mha.karimi@gmail.com>
2024-03-10 12:19:07 +0330

    Add number controller

---
+++ iCodeNext/DependencyPrinciples/Controllers/NumberController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyPrinciples.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class NumberController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok(5);
+    }
+}
